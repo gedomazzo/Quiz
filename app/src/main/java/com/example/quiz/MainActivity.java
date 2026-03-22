d2798e2ff02a5fc9c3e05df5cd65db974357b0bc
Vasya <br2876@bs.amalnet.k12.il>
2026-03-22 10:37:24 +0200

    testing reading raw files

--- app/src/main/java/com/example/quiz/MainActivity.java
+++ app/src/main/java/com/example/quiz/MainActivity.java
@@ -1,24 +1,56 @@
 package com.example.quiz;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
+    private final String FILENAME = "questions";
+
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        textView = findViewById(R.id.textView);
+
+
+        // Use the FILENAME directly as the resource name
+        int resourcedID = this.getResources().getIdentifier(FILENAME, "raw", this.getPackageName());
+
+        if (resourcedID != 0) {
+            try (InputStream iS = this.getResources().openRawResource(resourcedID);
+                 InputStreamReader isr = new InputStreamReader(iS);
+                 BufferedReader br = new BufferedReader(isr)) {
+
+                StringBuilder sB = new StringBuilder();
+                String line;
+                while ((line = br.readLine()) != null) {
+                    sB.append(line).append('\n');
+                }
+                
+                String text = sB.toString();
+                textView.setText(sB);
+                Log.d("QUIZ_DEBUG", "File content: " + text);
+                
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            Log.e("QUIZ_DEBUG", "Resource not found: " + FILENAME);
+        }
+
+
+
+
+
     }
 }
